864b627789c05171fad6433f444f3a64cb16e440
CIELO <cielodamarisangulo@gmail.com>
2026-05-28 10:38:45 -0500

    CFA

--- src/config/global.js
+++ src/config/global.js
@@ -239,7 +239,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/123103_CF07_DU.pdf',
       },
       {
         icono: 'fas fa-download',
